c1e0398caba8b5ceca897a3845aa2eaa7f143c88
Vivihcoelho <Vivihcoelho.06@gmail.com>
2024-11-22 09:11:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Ciencias',
+    'Qual o maior planeta do sistema solar?',
+    'Jupter'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Russia?',
+    'A capital da Russia é Moscou'
+)
+
+criaCartao(
+    'Programação',
+    'qual e a linguagem de programaçao mais utilizada no desenvolvimento web',
+    'JavaScript'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz casa em Inglês?',
+    'casa em ingles é House'
+)
